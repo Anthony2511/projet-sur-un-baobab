2437fbafb2b91d61a5d7617f42b56a5871d2b4a3
Anthony2511 <anthonybeaumecker@gmail.com>
2016-05-04 14:58:16 +0200

    add project wordpress

--- dev/wp-content/themes/Baobab/functions.php
+++ dev/wp-content/themes/Baobab/functions.php
@@ -1,5 +1,28 @@
 <?php
 
+/*
+ * Define post types
+ */
+
+ register_post_type( 'project', [
+     'label' => 'Court-métrages',
+     'labels' => [
+         'singular_name' => 'Court-métrage',
+         'add_new' => 'Ajouter un nouveau projet'
+     ],
+        'description' => 'La liste de tous les projets (court-métrages, ateliers,...) affichés sur le site',
+        'public' => true,
+        'menu_position' => 5,
+        'menu_icon' => 'dashicons-editor-video',
+        'supports' => ['title', 'editor', 'thumbnail'],
+        'has_archive' => true
+     ] );
+
+
+/*
+ * Generate an except based on custom rules, used on the home page
+ */
+
 function get_the_custom_excerpt($length = 150)
 {
       $excerpt = get_the_content();
