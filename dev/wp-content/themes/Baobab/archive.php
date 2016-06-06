be0a5baa6b6be8bf03b5c541b56029aa2486c7da
Anthony2511 <anthonybeaumecker@gmail.com>
2016-06-06 14:59:12 +0200

    added article page et list article

--- dev/wp-content/themes/Baobab/archive.php
+++ dev/wp-content/themes/Baobab/archive.php
@@ -22,72 +22,19 @@ get_header('home');
 </div>
 <div class="actus">
     <h2 aria-level="2" class="actus__title">Actualités</h2>
-        <a href="article.php" class="actus__link">
+    <?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+        <a href="<?php the_permalink();?>" class="actus__link">
             <div class="actus__link__container">
-                <img src="../img/article-actu1.jpg" alt="Projection de Safidn'i Pela" class="actus__link__images" width="624" height="416"/>
+                <img src="<?php the_field('article_image');?>" alt="Projection de Safidn'i Pela" class="actus__link__images" width="624" height="416"/>
                 <div class="actus__link__content">
-                    <span class="actus__link__date">Le 17 février 2016 à 20h31</span>
-                    <h3 aria-level="3" class="actus__link__title">Projection de Safidn'i Pela</h3>
-                    <a href="article.php" class="actus__link__view">Voir l'article</a>
+                    <span class="actus__link__date"><?php the_date();?></span>
+                    <h3 aria-level="3" class="actus__link__title"><?php the_title();?></h3>
+                    <a href="<?php the_permalink();?>" class="actus__link__view"><?php _e('Voir l\'article','b');?></a>
                 </div>
                 <div class="actus__link__opacity"></div>
             </div>
         </a>
-        <a href="article.php" class="actus__link">
-            <div class="actus__link__container">
-                <img src="../img/article-actu2.jpg" alt="Baobab à un an déja" class="actus__link__images" width="1280" height="853"/>
-                <div class="actus__link__content">
-                    <span class="actus__link__date">Le 17 février 2016 à 20h31</span>
-                    <h3 aria-level="3" class="actus__link__title">Sur un Baobab a un an !</h3>
-                    <a href="article.php" class="actus__link__view">Voir l'article</a>
-                </div>
-                <div class="actus__link__opacity"></div>
-            </div>
-        </a>
-        <a href="article.php" class="actus__link">
-            <div class="actus__link__container">
-                <img src="../img/article-actu1.jpg" alt="Projection de Safidn'i Pela" class="actus__link__images" width="624" height="416"/>
-                <div class="actus__link__content">
-                    <span class="actus__link__date">Le 17 février 2016 à 20h31</span>
-                    <h3 aria-level="3" class="actus__link__title">Projection de Safidn'i Pela</h3>
-                    <a href="article.php" class="actus__link__view">Voir l'article</a>
-                </div>
-                <div class="actus__link__opacity"></div>
-            </div>
-        </a>
-        <a href="article.php" class="actus__link">
-            <div class="actus__link__container">
-                <img src="../img/article-actu2.jpg" alt="Baobab à un an déja" class="actus__link__images" width="1280" height="853"/>
-                <div class="actus__link__content">
-                    <span class="actus__link__date">Le 17 février 2016 à 20h31</span>
-                    <h3 aria-level="3" class="actus__link__title">Sur un Baobab a un an !</h3>
-                    <a href="article.php" class="actus__link__view">Voir l'article</a>
-                </div>
-                <div class="actus__link__opacity"></div>
-            </div>
-        </a>
-        <a href="article.php" class="actus__link">
-            <div class="actus__link__container">
-                <img src="../img/article-actu1.jpg" alt="Projection de Safidn'i Pela" class="actus__link__images" width="624" height="416"/>
-                <div class="actus__link__content">
-                    <span class="actus__link__date">Le 17 février 2016 à 20h31</span>
-                    <h3 aria-level="3" class="actus__link__title">Projection de Safidn'i Pela</h3>
-                    <a href="article.php" class="actus__link__view">Voir l'article</a>
-                </div>
-                <div class="actus__link__opacity"></div>
-            </div>
-        </a>
-        <a href="article.php" class="actus__link">
-            <div class="actus__link__container">
-                <img src="../img/article-actu2.jpg" alt="Baobab à un an déja" class="actus__link__images" width="1280" height="853"/>
-                <div class="actus__link__content">
-                    <span class="actus__link__date">Le 17 février 2016 à 20h31</span>
-                    <h3 aria-level="3" class="actus__link__title">Sur un Baobab a un an !</h3>
-                    <a href="article.php" class="actus__link__view">Voir l'article</a>
-                </div>
-                <div class="actus__link__opacity"></div>
-            </div>
-        </a>
+    <?php endwhile; endif; ?>
     <div class="pictures__pagination">
         <a href="#" class="pictures__link pictures__link--active">1</a>
         <a href="#" class="pictures__link">2</a>
--- dev/wp-content/themes/Baobab/single-post.php
+++ dev/wp-content/themes/Baobab/single-post.php
@@ -1,55 +1,36 @@
 <?php
 /*
-      Template Name: Single Article
+      Template Name: Single Post
 */
 get_header('home');
 
 ?>
+<main class="container__size">
             <div class="fil-ariane__size">
                 <p class="fil-ariane__chemin">
-                    <a href="index.php" class="fil-ariane__link">Accueil</a> >
-                    <a href="actualités.php" class="fil-ariane__link">Actualités</a> >
-                    <a href="article.php" class="fil-ariane__link">Sur un Baobab a un an</a>
+                    <a href="<?php echo get_home_url(); ?>" class="fil-ariane__link">Accueil</a> >
+                    <a href="<?php the_permalink(84);?>" class="fil-ariane__link">Actualités</a> >
+                    <a href="<?php get_permalink();?>" class="fil-ariane__link"><?php the_title();?></a>
                 </p>
             </div>
             <div class="article">
-                <h2 aria-level="2" class="article__title"> Sur un Baobab a un an !</h2>
-                <span class="article__date">Le 15 février 2016 à 15h</span>
-                <img src="img/article.jpg" class="article__image" alt="Sur un Baobab a un an" width="1000" height="667"/>
+                <h2 aria-level="2" class="article__title"><?php the_title();?></h2>
+                <span class="article__date"><?php the_date();?></span>
+                <img src="<?php the_field('article_image');?>" class="article__image" alt="Sur un Baobab a un an" width="1000" height="667"/>
                 <div class="article__texte">
                     <div class="article__partie-one">
                         <p class="article__paragraphe">
-                            Il y a juste un an, François et moi partions seuls pour Madagascar
-                            avec pour unique certitude l’envie commune de réaliser un projet où
-                            se rejoignent nos passions pour l’éducation et le cinéma d’animation.
-                            Vouloir faire des dessin animés avec des enfants à l’autre bout du monde
-                            semblait être une idée un peu folle surtout en voyageant sac-à-dos, en
-                            taxi-brousse, à pied ou en stop. Mais cela ne nous a pas démotivé bien
-                            au contraire. Au fil de nos rencontres et des échanges, cette aventure nous
-                            a permis de découvrir de nombreuses facettes de Madagascar.
+                            <?php the_field('article_section-one_paragraphe-one');?>
                         </p>
                         <p class="article__paragraphe article__paragraphe--float">
-                            Même si elles sont souvent positives, nous avons été confronté à des réalités
-                            plus dures comme le la corruption omniprésente ou l’extrême pauvreté de la plupart
-                            des grandes villes. Cela nous a renforcé dans notre volonté de lier notre projet
-                            au soutien d’une cause sociale locale. Au delà de la rencontre avec des jeunes
-                            malgaches, le projet nous a surtout permis de découvrir et de partager leur quotidien.
-                            Enfin, ce fut l’occasion de rencontrer plusieurs ONG locales et internationales auxquelles
-                            nous proposions de réaliser un atelier qui s’inscrivait au programme de leurs activités.
+                            <?php the_field('article_section-one_paragraphe-two');?>
                         </p>
-                        <h3 aria-level="3" class="article__partie-one__title">La suite ...</h3>
+                        <h3 aria-level="3" class="article__partie-one__title"><?php the_field('article_title-section-two');?></h3>
                         <p class="article__paragraphe">
-                            Après plusieurs mois sur place et la réalisation de deux ateliers nous voilà de retour
-                            encore plus motivés pour de nouveaux projets. Au programme de 2016, nous avons d’abord
-                            la préparation d’un nouvel atelier depuis la Belgique qui se fera probablement de nouveau
-                            à Madagascar. Nous continuons également de diffuser un maximum les films réalisés par
-                            les enfants de Tuléar en 2015 sur le tourisme responsable et la grossesse précoce au delà de Madagascar.
+                            <?php the_field('article_section-two_paragraphe-one');?>
                         </p>
                         <p class="article__paragraphe article__paragraphe--float">
-                            Nous cherchons et contactons des festivals de courts-métrages ou même des associations prêtes
-                            à diffuser les films. Enfin, nous préparons également une petite exposition qui sera l’occasion
-                            de partager et de présenter quelques images de ce qu’il y a autour des projets réalisés à Madagascar
-                            (déroulement de l’atelier, interviews de participants, photos, etc…).
+                            <?php the_field('article_section-two_paragraphe-two');?>
                         </p>
                     </div>
                 </div>
@@ -74,70 +55,5 @@ get_header('home');
                     </p>
                 </div>
             </div>
-        </main>
-        <footer class="footer">
-            <div class="footer__opacity">
-                <div class="footer__section-one">
-                    <div class="footer__section-one__contact">
-                        <h2 aria-level="2" class="footer__section-one__title">Nous contacter</h2>
-                        <p class="footer__section-one__numero">
-                            WhatsApp : +32495348966
-                        </p>
-                        <p class="footer__section-one__numero">
-                            Téléphone : +32495348966
-                        </p>
-                    </div>
-                    <div class="footer__section-one__contact">
-                        <h2 aria-level="2" class="footer__section-one__title">Dernières nouvelles</h2>
-                        <a href="article.php" class="footer__section-one__nouvelles">
-                            Sur un baobab, un an déja !
-                        </a>
-                        <a href="article.php" class="footer__section-one__nouvelles">
-                            Projection de Safidn’i Pela
-                        </a>
-                        <a href="article.php" class="footer__section-one__nouvelles">
-                            Sur un baobab, un an déja !
-                        </a>
-                    </div>
-                    <div class="footer__section-one__contact">
-                        <h2 aria-level="2" class="footer__section-one__title">Navigation</h2>
-                        <ul class="footer__section-one__list">
-                            <li class="footer__section-one__detail"><a href="index.php" class="footer__section-one__link">Accueil</a></li>
-                            <li class="footer__section-one__detail"><a href="projets.php" class="footer__section-one__link">Projets</a></li>
-                            <li class="footer__section-one__detail"><a href="actualités.php" class="footer__section-one__link">Actualités</a></li>
-                            <li class="footer__section-one__detail"><a href="ateliers.php" class="footer__section-one__link">Ateliers</a></li>
-                            <li class="footer__section-one__detail"><a href="propos.php" class="footer__section-one__link">A Propos</a></li>
-                            <li class="footer__section-one__detail"><a href="contact.php" class="footer__section-one__link">Contact</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="footer__section-two">
-                    <div class="footer__section-two__copyright">
-                        <p class="footer__section-two__date">
-                            &copy; 2016 | Design by Baobab
-                        </p>
-                    </div>
-                    <div class="footer__section-two__social">
-                        <a href="#" class="footer__section-two__link">
-                            <img src="img/Youtube.png" class="footer__section-two__logo"alt="Youtube" width="167" height="167"/>
-                        </a>
-                        <a href="#" class="footer__section-two__link">
-                            <img src="img/Twitter.png" class="footer__section-two__logo"alt="Twitter" width="167" height="167"/>
-                        </a>
-                        <a href="#" class="footer__section-two__link">
-                            <img src="img/Facebook.png" class="footer__section-two__logo"alt="Facebook" width="167" height="167"/>
-                        </a>
-                        <a href="#" class="footer__section-two__link">
-                            <img src="img/Instagram.png" class="footer__section-two__logo"alt="Instagram" width="167" height="167"/>
-                        </a>
-                    </div>
-                    <div class="footer__section-two__back">
-                        <a href="#" class="footer__section-two__high">Revenir en haut</a>
-                    </div>
-                </div>
-            </div>
-        </footer>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"/></script>
-        <script type="text/javascript" src="javascript.js"/></script>
-    </body>
-</html>
+            <?php
+                get_footer();
